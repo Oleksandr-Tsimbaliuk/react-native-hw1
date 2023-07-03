c8dc1764e4d154b41b76d8d9ec03c064d7ccac3c
Oleksandr <115025828+Oleksandr-Tsimbaliuk@users.noreply.github.com>
2023-07-03 14:37:09 +0300

    Update

--- AwesomeProject/src/Screens/RegistrationScreen/RegistrationScreen.jsx
+++ AwesomeProject/src/Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -12,13 +12,15 @@ import {
   Keyboard,
   ImageBackground,
 } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { styles } from "./StyledRegistrationScreen";
 import InputRegistration from "../../components/InputRegistration/InputRegistration";
 import imageBackground from "../../../assets/images/app_background.png";
 
-
 const RegistrationScreen = () => {
+  const navigation = useNavigation();
+
   const [userImage, setUserImage] = useState("");
   const [login, setLogin] = useState("");
   const [email, setEmail] = useState("");
@@ -31,6 +33,7 @@ const RegistrationScreen = () => {
   };
   const handleFormSubmitButton = () => {
     console.log(login, email, password);
+    navigation.navigate("Home");
   };
   return (
     <ImageBackground
@@ -38,70 +41,71 @@ const RegistrationScreen = () => {
       resizeMode="cover"
       style={styles.imageBackground}
     >
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.registrationContainer}>
-        <View style={styles.userImageContainer}>
-          {/* <Image source={{ uri: "" }}></Image> */}
-          <Button onPress={handleAddUserImage} title="Add photo" />
-          <Button onPress={handleRemoveUserImage} title="Remove photo" />
-        </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.registrationContainer}>
+          <View style={styles.userImageContainer}>
+            {/* <Image source={{ uri: "" }}></Image> */}
+            <Button onPress={handleAddUserImage} title="Add photo" />
+            <Button onPress={handleRemoveUserImage} title="Remove photo" />
+          </View>
 
-        <Text style={styles.registrationFormHeader}> Реєстрація</Text>
+          <Text style={styles.registrationFormHeader}> Реєстрація</Text>
           <KeyboardAvoidingView
             behavior={Platform.OS == "ios" ? "padding" : "height"}
             // style={styles.container}
             // keyboardVerticalOffset={-255}
           >
-        <View style={styles.registrationForm}>
-            <InputRegistration
-              name={"login"}
-              type={"text"}
-              value={login}
-              placeholder="Логін"
-              onChangeText={setLogin}
-            ></InputRegistration>
-            <InputRegistration
-              name={"email"}
-              type={"email"}
-              value={email}
-              placeholder="Адреса електронної пошти"
-              onChangeText={setEmail}
-            ></InputRegistration>
-            <InputRegistration
-              name={"password"}
-              type={"password"}
-              value={password}
-              placeholder="Пароль"
-              onChangeText={setPassword}
-            ></InputRegistration>
-        </View>
+            <View style={styles.registrationForm}>
+              <InputRegistration
+                name={"login"}
+                type={"text"}
+                value={login}
+                placeholder="Логін"
+                onChangeText={setLogin}
+              ></InputRegistration>
+              <InputRegistration
+                name={"email"}
+                type={"email"}
+                value={email}
+                placeholder="Адреса електронної пошти"
+                onChangeText={setEmail}
+              ></InputRegistration>
+              <InputRegistration
+                name={"password"}
+                type={"password"}
+                value={password}
+                placeholder="Пароль"
+                onChangeText={setPassword}
+              ></InputRegistration>
+            </View>
           </KeyboardAvoidingView>
-        <TouchableOpacity
-          style={styles.registrationFormSubmitButton}
-          onPress={handleFormSubmitButton}
-          title="Зареєструватися"
-        >
+          <TouchableOpacity
+            style={styles.registrationFormSubmitButton}
+            onPress={handleFormSubmitButton}
+            title="Зареєструватися"
+          >
+            <Text
+              style={{
+                fontSize: 16,
+                textAlign: "center",
+                color: "#ffffff",
+              }}
+            >
+              Зареєструватися
+            </Text>
+          </TouchableOpacity>
           <Text
             style={{
               fontSize: 16,
               textAlign: "center",
-              color: "#ffffff",
+              color: "#1B4371",
             }}
+            onPress={() => navigation.navigate("Login")}
           >
-            Зареєструватися
+            Вже є акаунт? Увійти
           </Text>
-        </TouchableOpacity>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: "center",
-            color: "#1B4371",
-          }}
-        >
-          Вже є акаунт? Увійти
-        </Text>
-      </View>
-    </TouchableWithoutFeedback>
+        </View>
+      </TouchableWithoutFeedback>
     </ImageBackground>
   );
 };
