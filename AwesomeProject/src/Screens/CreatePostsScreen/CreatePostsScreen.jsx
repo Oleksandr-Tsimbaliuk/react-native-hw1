16ddd653fdd40241ce2eec9287704202fba998cd
Oleksandr <115025828+Oleksandr-Tsimbaliuk@users.noreply.github.com>
2023-07-08 22:29:03 +0300

    Update CreatePostsScreen.jsx

--- AwesomeProject/src/Screens/CreatePostsScreen/CreatePostsScreen.jsx
+++ AwesomeProject/src/Screens/CreatePostsScreen/CreatePostsScreen.jsx
@@ -5,6 +5,7 @@ import * as MediaLibrary from "expo-media-library";
 import { Ionicons } from "@expo/vector-icons";
 // import { TextInput } from "react-native-gesture-handler";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import {
   Text,
@@ -26,7 +27,11 @@ const CreatePostsScreen = () => {
   const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   // const [isMakingPhoto, setIsMakingPhoto] = useState(true);
-  const [inputTitle, setInputTitle] = useState("");
+  const [inputPhotoTitle, setInputPhotoTitle] = useState("");
+  // const [inputPhotoLocation, setInputPhotoLocation] = useState("");
+  const [location, setLocation] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [city, setCity] = useState(null);
   const [photo, setPhoto] = useState(null);
   // const [post, setPost] = useState({
   //   title: "",
@@ -43,6 +48,22 @@ const CreatePostsScreen = () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       await MediaLibrary.requestPermissionsAsync();
       setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("У доступі до місцезнаходження відмовлено");
+        return;
+      }
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      const coords = {
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+      };
+      setLocation(coords);
     })();
   }, []);
 
@@ -94,6 +115,26 @@ const CreatePostsScreen = () => {
   //   navigation.navigate("PostsScreen");
   // };
 
+  const getCityAndCountry = async () => {
+    if (location) {
+      try {
+        const { latitude, longitude } = location;
+        const response = await fetch(
+          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&accept-language=uk`
+        );
+        const data = await response.json();
+        if (data.address) {
+          const locationCity = data.address.city;
+          const locationCountry = data.address.country;
+          setCity(locationCity);
+          setCountry(locationCountry);
+        }
+      } catch (error) {
+        alert(`Місцезнаходження не визначене`);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       {photo ? (
@@ -143,13 +184,37 @@ const CreatePostsScreen = () => {
       </TouchableOpacity>
 
       <TextInput
+        style={styles.photoMetaInput}
         placeholder="Назва..."
         type="text"
         required
         name="photo"
-        value={inputTitle}
-        onChangeText={setInputTitle}
+        value={inputPhotoTitle}
+        onChangeText={setInputPhotoTitle}
       />
+
+      {/* <TextInput
+        style={styles.photoMetaInput}
+        placeholder="Місцевість"
+        type="text"
+        required
+        name="Місцевість"
+        value={inputPhotoLocation}
+        onChangeText={setInputPhotoLocation}
+      >
+        <TouchableOpacity></TouchableOpacity>
+      </TextInput> */}
+
+      <View style={styles.photoMetaLocation}>
+        <TouchableOpacity style={styles.areaButton} onPress={getCityAndCountry}>
+          <Ionicons name="location-outline" size={24} color="#BDBDBD" />
+          {country ? (
+            <Text style={styles.areaButtonText}>{`${city}, ${country}`}</Text>
+          ) : (
+            <Text style={styles.areaButtonText}>Місцевість</Text>
+          )}
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -218,6 +283,48 @@ const styles = StyleSheet.create({
     lineHeight: 19,
     color: "#BDBDBD",
     marginTop: 8,
+  },
+
+  photoMetaInput: {
+    display: "flex",
+    alignItems: "center",
+
+    width: "100%",
+    height: 50,
+    marginTop: 32,
+
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    color: "#BDBDBD",
+
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    borderBottomWidth: 1,
+  },
+
+  photoMetaLocation: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+
+    width: "100%",
+    height: 50,
+    marginTop: 16,
+
+    borderStyle: "solid",
+    borderColor: "#E8E8E8",
+    borderBottomWidth: 1,
+  },
+  areaButton: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 2,
+  },
+  areaButtonText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    color: "#BDBDBD",
   },
 });
 
